29dfe636fc5eb8d137b8762b32f08f03bc791be2
YoussefSS <joesamir98@gmail.com>
2023-03-23 12:35:41 +0200

    Added / (root), /signin and /register routes

--- server.js
+++ server.js
@@ -2,6 +2,58 @@ import express from 'express';
 
 const app = express();
 
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0, // how many times this user submitted a photo
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0, // how many times this user submitted a photo
+            joined: new Date()
+        }
+    ]
+}
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success');
+    }
+    else{
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    database.users.push({
+        id: '125',
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        entries: 0,
+        joined: new Date()
+    })
+
+    res.json(database.users[database.users.length-1]);
+})
+
+
 // In the listen function we can have a secondary parameter that's a function that happens after the listen happens
 app.listen(3000, () => {
     console.log('app is running on port 3000');
