b51dbf05ed0b5dc1609e11610afa8e3530144cad
YoussefSS <joesamir98@gmail.com>
2023-03-26 16:34:16 +0200

    Storing password hashes when registering + login

--- server.js
+++ server.js
@@ -18,34 +18,6 @@ const db = knex({
 
 const app = express();
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-            entries: 0, // how many times this user submitted a photo
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'bananas',
-            email: 'sally@gmail.com',
-            entries: 0, // how many times this user submitted a photo
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-
 app.use(express.json());
 app.use(cors());
 
@@ -54,39 +26,49 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-
-    bcrypt.compare("apples", '$2a$10$kKJTIJAhrYg3bLF0Uxc8V.Pt3zihgXfAoa0UjBjcwVXVMeUSU8GbO', (err, res) => { // correct password, will return true in res
-        console.log('first guess', res);
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then((data) => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if(isValid) {
+            return db.select('*').from('users')
+                .where('email', '=', req.body.email)
+                .then((user) => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('unable to get user'));
+        }
+        else {
+            res.status(400).json('wrong credentials');
+        }
     })
-
-    bcrypt.compare("vegies", '$2a$10$kKJTIJAhrYg3bLF0Uxc8V.Pt3zihgXfAoa0UjBjcwVXVMeUSU8GbO', (err, res) => { // wrong password, will return false in res
-        console.log('second guess', res);
-    })
-
-    if(req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-    }
-    else{
-        res.status(400).json('error logging in');
-    }
+    .catch(err => res.status(400).json('wrong credentials'));
 })
 
 app.post('/register', (req, res) => {
+    const bcryptHash = bcrypt.hashSync(req.body.password);
 
-    bcrypt.hash(req.body.password, null, null, (err, hash) => {
-        console.log(hash);
-    })
-
-    db('users')
-    .returning('*')
-    .insert({
-        email: req.body.email,
-        name: req.body.name,
-        joined: new Date()
-    })
-    .then((user) => { // response is the result of .returning
-        res.json(user[0]); // returns an array of 1 item, so get the first item
+    db.transaction((trx) => {
+        trx.insert({
+            hash: bcryptHash,
+            email: req.body.email
+        })
+        .into('login')
+        .returning('email')
+        .then((loginEmail) => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: req.body.name,
+                joined: new Date()
+            })
+            .then((user) => { // response is the result of .returning
+                res.json(user[0]); // returns an array of 1 item, so get the first item
+            })
+        })
+        .then(trx.commit) // If the transaction is all good, then commit
+        .catch(trx.rollback) // If there was a problem with the transaction, ie one of the operations failed, rollback everything you did
     })
     .catch((err) => {
         res.status(400).json('unable to register'); // if you print out err, it will tell you that email already exists for example
@@ -113,7 +95,6 @@ app.get('/profile/:id', (req, res) => {
 })
 
 app.put('/image', (req, res) => {
-    
     db('users').where('id', '=', req.body.id)
     .increment('entries', 1)
     .returning('entries')
@@ -127,7 +108,6 @@ app.put('/image', (req, res) => {
     /*.update({
         entries:  // We use increment instead of grabbing the entries manually then incrementing it
     })*/
-
 })
 
 
